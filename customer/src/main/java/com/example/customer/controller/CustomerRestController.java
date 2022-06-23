fa2f90f59b0d7feb044db499acad2f9da8335361
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2022-06-23 16:33:55 -0500

    Clientes

---
+++ customer/src/main/java/com/example/customer/controller/CustomerRestController.java
@@ -0,0 +1,53 @@
+package com.example.customer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.customer.entities.Customer;
+import com.example.customer.repository.CustomerRepository;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerRestController {
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@GetMapping()
+	public List<Customer> list(){
+		return customerRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Customer get(@PathVariable String id) {
+		return null;
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> put(@PathVariable String id, @RequestBody Customer input){
+		return null;
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> post(@RequestBody Customer input){
+		Customer save = customerRepository.save(input);
+		return ResponseEntity.ok(save);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable String id){
+		return null;
+	}
+
+	
+}
---
+++ customer/src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.customer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.customer.entities.Customer;
+
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+
+}
